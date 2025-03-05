22a7c4f3514b26b2a161676b3520d726ad9cae5d
zoeburke <147422821+zoeburke@users.noreply.github.com>
2025-03-05 18:35:50 +0000

    Update lightdisplay.py

--- lightdisplay.py
+++ lightdisplay.py
@@ -1,10 +1,10 @@
 import time
+from random import randint
 from adafruit_circuitplayground import cp
-
 
 class LightDisplay:
     BLACK = [0, 0, 0]
-    NUM_PIXELS = len(cp.pixels)  # Typically 10 neopixels on the board
+    NUM_PIXELS = len(cp.pixels)
 
     def __init__(self, brightness):
         cp.pixels.brightness = brightness
@@ -27,12 +27,29 @@ class LightDisplay:
                 cp.pixels[index] = colour
         cp.pixels.show()
 
+    def random_light(self, colour, sec_interval):
+        for _ in range(5):
+            indices = []
+            while len(indices) < 5:
+                num = randint(0, self.NUM_PIXELS - 1)
+                if num not in indices:
+                    indices.append(num)
+            for i in indices:
+                cp.pixels[i] = colour
+            cp.pixels.show()
+            time.sleep(sec_interval)
+            for i in indices:
+                cp.pixels[i] = self.BLACK
+            cp.pixels.show()
+            time.sleep(sec_interval)
+
     def snake(self, snake_size, colour, interval):
         if snake_size < 2 or snake_size > self.NUM_PIXELS // 2:
             return
         for i in range(self.NUM_PIXELS - snake_size + 1):
             cp.pixels.fill(self.BLACK)
-            for j in range(snake_size):
-                cp.pixels[i + j] = colour
+            for k in range(snake_size):
+                cp.pixels[i + k] = colour
             cp.pixels.show()
             time.sleep(interval)
+
